e859ea299ee60259e0888cb4a67aef6710ebc4a4
MayconEidam <127507968+MayconEidam@users.noreply.github.com>
2023-10-20 10:43:59 -0300

    ok

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+//Declaração/criação da função tocaSomMsg
 function tocaSomMsg(){
     document.querySelector('#som_tecla_msg').play();
 }
+//chamada ou invocação da função tocaSomMsg ao clicar no botão
+document.querySelector('.tecla_msg').onclick=tocaSomMsg;
 
 
