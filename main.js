f9a843a8fa86869f8eacb9942bb0c570f11656a8
MayconEidam <127507968+MayconEidam@users.noreply.github.com>
2023-11-10 11:02:48 -0300

    ok

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //Declaração/criação da função tocaSomMsg
-function tocaSom (idElementoAudio){
+function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 
 
-for(let contador = 0;c ontador < ListaDeTeclas.length; contador++){
+for(let contador = 0; contador < ListaDeTeclas.length; contador++){
     const teclas = ListaDeTeclas [contador]
     const efeito = teclas.classList[1];
     const idAudio = `#som_${efeito}`;
