4ec6a10be9eae810cdb5e878f84b66a38d2f5f39
MayconEidam <127507968+MayconEidam@users.noreply.github.com>
2023-11-10 10:54:38 -0300

    ok

--- main.js
+++ main.js
@@ -1,67 +1,18 @@
 //Declaração/criação da função tocaSomMsg
-function tocaSomMsg(){
-    document.querySelector('#som_tecla_msg').play();
+function tocaSomMsg(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
-//chamada ou invocação da função tocaSomMsg ao clicar no botão
-document.querySelector('.tecla_msg').onclick = tocaSomMsg;
 
-//Declaração/criação da função tocaSomGmd
-function tocaSomGmd(){
-    document.querySelector('#som_tecla_gmd').play();
-}
-//chamada ou invocação da função tocaSomGmd ao clicar no botão
-document.querySelector('.tecla_gmd').onclick = tocaSomGmd;
-
-//Declaração/criação da função tocaSomLaele
-function tocaSomLaele(){
-    document.querySelector('#som_tecla_laele').play();
-}
-//chamada ou invocação da função tocaSomLaele ao clicar no botão
-document.querySelector('.tecla_laele').onclick = tocaSomLaele;
-
-//Declaração/criação da função tocaSomUepa
-function tocaSomUepa(){
-    document.querySelector('#som_tecla_uepa').play();
-}
-//chamada ou invocação da função tocaSomUepa ao clicar no botão
-document.querySelector('.tecla_uepa').onclick = tocaSomUepa;
-
-//Declaração/criação da função tocaSomOmg
-function tocaSomOmg(){
-    document.querySelector('#som_tecla_omg').play();
-}
-//chamada ou invocação da função tocaSomOmg ao clicar no botão
-document.querySelector('.tecla_omg').onclick = tocaSomOmg;
-
-//Declaração/criação da função tocaSomSad
-function tocaSomSad(){
-    document.querySelector('#som_tecla_sad').play();
-}
-//chamada ou invocação da função tocaSomSad ao clicar no botão
-document.querySelector('.tecla_sad').onclick = tocaSomSad;
-
-//Declaração/criação da função tocaSomHahaha
-function tocaSomHahaha(){
-    document.querySelector('#som_tecla_hahaha').play();
-}
-//chamada ou invocação da função tocaSomHahaha ao clicar no botão
-document.querySelector('.tecla_hahaha').onclick = tocaSomHahaha;
-
-//Declaração/criação da função tocaSomPaysandu
-function tocaSomPaysandu(){
-    document.querySelector('#som_tecla_paysandu').play();
-}
-//chamada ou invocação da função tocaSomPaysandu ao clicar no botão
-document.querySelector('.tecla_paysandu').onclick = tocaSomPaysandu;
-
-//Declaração/criação da função tocaSomVasco
-function tocaSomVasco(){
-    document.querySelector('#som_tecla_vasco').play();
-}
-//chamada ou invocação da função tocaSomVasco ao clicar no botão
-document.querySelector('.tecla_vasco').onclick = tocaSomVasco;
-
-//criando a referências constante ListaDeTeclas e buscando a classe coletiva .tecla
 const ListaDeTeclas = document.querySelectorAll('.tecla');
-//chamando a constante ListaDeTeclas e o índice 0 do somMsg
-ListaDeTeclas[0].onclick = tocaSomMsg;
+
+let contador = 0;
+while (contador < ListaDeTeclas.length){
+    const teclas = ListaDeTeclas [contador]
+    const efeito = teclas.classList[1];
+    const idAudio = `"#som_"${efeito}`;
+    teclas.onclick = function(){
+        tocaSomMsg(idAudio);
+    }
+ contador = contador + 1;
+ console.log(contador);   
+}
